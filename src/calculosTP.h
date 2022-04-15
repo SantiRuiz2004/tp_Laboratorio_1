424ecddaa014bafad22455f31b4796824d49c4b0
SantiRuiz2004 <ruizdiazsantiago44@gmail.com>
2022-04-15 12:11:19 -0300

    Trabajo Practico

--- src/calculosTP.h
+++ src/calculosTP.h
@@ -14,4 +14,19 @@ float bitcoin(float precio);
 float precioKm(float precio, int kilometros);
 float diferenciaPrecio(float precioUno, float precioDos);
 
+//float debito(float precio)
+// realiza el calculo del descuento
+
+//float credito(float precio)
+//realiza el calculo de interes
+
+//float bitcoin (precio)
+// realiza la conversion del precio en bitcoin
+
+//float precioKm(float precio)
+//realiza el calculo del precio por Kilometro
+
+//diferencaPrecio(float, float)
+//realiza realiza el calculo de resta para saber la diferencia  de precios
+
 #endif /* CALCULOSTP_H_ */
--- src/datos.h
+++ src/datos.h
@@ -10,4 +10,10 @@
 
 int datos(float debitoLatam, float creditoLatam, float debitoAerolineas, float creditoAerolineas, float precioBitcoinLatam, float precioBitcoinAerolineas, float precioPorKmLatam, float precioPorKmAerolineas, int kilometrosIngresados, float diferenciaPrecios, float precioDelVueloLatam,float precioDelVueloAerolineas);
 int menuOpciones(void);
+
+//int menuOpciones(void)
+//muestra el menu de opciones
+
+//int datos
+//float y variables usadas en el trabajo practico
 #endif /* DATOS_H_ */
