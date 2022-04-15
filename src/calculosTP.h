46cfd685a685f60f2b1ae101cda172a4db53a5ea
SantiRuiz2004 <ruizdiazsantiago44@gmail.com>
2022-04-15 12:14:58 -0300

    trabajo Practico 1

--- src/calculosTP.h
+++ src/calculosTP.h
@@ -14,19 +14,35 @@ float bitcoin(float precio);
 float precioKm(float precio, int kilometros);
 float diferenciaPrecio(float precioUno, float precioDos);
 
-//float debito(float precio)
-// realiza el calculo del descuento
+///@fn float debito(float precio)
+///@brief realiza el calculo de descuento con tarjeta de debito
+///
+///@param debitoLatam,debitoAerolineas
+///@descuento retorna la opcion seleccionada float debito(float precio)
 
-//float credito(float precio)
-//realiza el calculo de interes
 
-//float bitcoin (precio)
-// realiza la conversion del precio en bitcoin
+///@fn float credito(float,float)
+///@brief realiza el calculo de interes con tarjeta de credito
+///
+///@param creditoLatam,creditoAerolineas
+///@credito1 retorna la opcion seleccionada float credito(float precio)
 
-//float precioKm(float precio)
-//realiza el calculo del precio por Kilometro
 
-//diferencaPrecio(float, float)
-//realiza realiza el calculo de resta para saber la diferencia  de precios
+///@fn float bitcoin (float precio)
+///@brief realiza la conversion del precio en bitcoin
+///
+///@param precioBitcoinLatam,precioBitcoinAerolineas
+///@bitcoinUno retorna a la opcion seleccionada float bitcoin(float precio)
 
+///@fn float precioKm(float precio)
+///@brief realiza el calculo del precio por Kilometro
+///
+///@param kilometrosIngresados, precioDelvueloLatam,precioDelvueloAerolineas
+///@precioKmUno retorna la opcion seleccionada float precioKm(precio)
+
+///@fn float diferencaPrecio(float, float)
+///@brief realiza el calculo de resta para saber la diferencia  de precios
+///
+///@param precioDelvueloLatam,precioDelvueloAerolineas
+///@diferencia retorna a la opcion seleccionada float diferenciaPrecio(float,float)
 #endif /* CALCULOSTP_H_ */
--- src/datos.h
+++ src/datos.h
@@ -11,9 +11,16 @@
 int datos(float debitoLatam, float creditoLatam, float debitoAerolineas, float creditoAerolineas, float precioBitcoinLatam, float precioBitcoinAerolineas, float precioPorKmLatam, float precioPorKmAerolineas, int kilometrosIngresados, float diferenciaPrecios, float precioDelVueloLatam,float precioDelVueloAerolineas);
 int menuOpciones(void);
 
-//int menuOpciones(void)
-//muestra el menu de opciones
+///@fn int menuOpciones(void)
+///@brief muestra el menu de opciones
+///
+///@param
+///@return retorna a la opcion seleccionada int menuOpciones(void)
 
-//int datos
-//float y variables usadas en el trabajo practico
+
+///@fn int datos(float,float,float,float,float,float,float,float,float,float,float,int)
+///@brief muestras los datos ingresados
+///
+///@param kilometrosIngresados,precioDelVueloLatam, debitoLatam, creditoLatam, precioBitcoinLatam, precioPorKmLatam, precioDelVueloAerolineas, debitoAerolineas, creditoAerolineas, precioBitcoinAerolineas, precioPorKmAerolineas, diferenciaPrecios
+///@return retorna a la opcion seleccionada int datos(float,float,float,float,float,float,float,float,float,float,float,int)
 #endif /* DATOS_H_ */
